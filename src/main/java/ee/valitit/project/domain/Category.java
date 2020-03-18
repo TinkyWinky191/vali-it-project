8a6cb838c48ac23e14a4ea1ec0ab836b02140660
andreivaino@live.ru <andreivaino@live.ru>
2020-03-18 15:40:18 +0200

    Created Category, Material, Note, Theme POJOS

--- src/main/java/ee/valitit/project/domain/Category.java
+++ src/main/java/ee/valitit/project/domain/Category.java
@@ -4,14 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(name="category")
-public class Category {
+@Entity(name = "category")
+public class Category extends AuditableEntity{
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name can't be empty or null")
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
 
 }
--- src/main/java/ee/valitit/project/domain/Note.java
+++ src/main/java/ee/valitit/project/domain/Note.java
@@ -1,5 +1,27 @@
 package ee.valitit.project.domain;
 
-public class Note extends AuditableEntity {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "note")
+public class Note extends AuditableEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name can't be empty or null")
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
 
 }
