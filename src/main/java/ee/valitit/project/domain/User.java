8fd6fcacd97d74bc7242e5742c70c5d441df6942
andreivaino@live.ru <andreivaino@live.ru>
2020-03-18 13:53:33 +0200

    User's email validation pattern added in User POJO.

--- src/main/java/ee/valitit/project/domain/User.java
+++ src/main/java/ee/valitit/project/domain/User.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 @NoArgsConstructor
@@ -31,6 +32,8 @@ public class User {
     @Column(name = "last_name")
     private String lastName;
 
+    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^-]+(?:\\\\.[a-zA-Z0-9_!#$%&’*+/=?`{|}~^-]+)*@[a-zA-Z0-9-]+(?:\\\\.[a-zA-Z0-9-]+)*$"
+            , message = "Not a valid email!")
     @NotBlank(message = "Email can't be empty or null")
     @Column(name = "email")
     private String email;
